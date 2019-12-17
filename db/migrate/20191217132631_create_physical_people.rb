47e32e407113f72545e514731df313b3437879a2
GabrielGQA <gabrielgqa@gmail.com>
2019-12-17 10:26:55 -0300

    Gestão de pessoa física

---
+++ db/migrate/20191217132631_create_physical_people.rb
@@ -0,0 +1,13 @@
+class CreatePhysicalPeople < ActiveRecord::Migration[6.0]
+  def change
+    create_table :physical_people do |t|
+      t.char :gender
+      t.string :cpf
+      t.string :rg
+      t.date :birth_date
+      t.references :client, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
